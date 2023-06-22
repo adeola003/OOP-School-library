af1bfd9d721a1c7167641a3162aa5700d4fa7ad3
adeola003 <khadaf3@gmail.com>
2023-06-22 13:54:35 +0100

    refactor person class and add inheritance

--- person.rb
+++ person.rb
@@ -1,4 +1,5 @@
-class Person
+require_relative 'nameable'
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
@@ -7,6 +8,10 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   def can_use_services?
