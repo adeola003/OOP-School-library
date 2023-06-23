8532edda0b678054ce53d0831fbe037b779f23fa
adeola003 <khadaf3@gmail.com>
2023-06-23 12:23:03 +0100

    fix rubocop errors

--- app.rb
+++ app.rb
@@ -1,7 +1,3 @@
-require_relative 'book'
-require_relative 'person'
-require_relative 'rental'
-
 class App
   def initialize
     @books = []
@@ -11,42 +7,40 @@ class App
 
   def run
     puts 'Welcome to the Console Library App!'
-
     loop do
-      puts "\nPlease choose an option:"
-      puts '1. List all books'
-      puts '2. List all people'
-      puts '3. Create a person'
-      puts '4. Create a book'
-      puts '5. Create a rental'
-      puts '6. List rentals for a person'
-      puts '7. Quit'
-
+      display_options
       choice = gets.chomp.to_i
-
-      case choice
-      when 1
-        list_all_books
-      when 2
-        list_all_people
-      when 3
-        create_person
-      when 4
-        create_book
-      when 5
-        create_rental
-      when 6
-        list_rentals_for_person
-      when 7
-        puts 'Thank you for using the Console Library App. Goodbye!'
-        break
-      else
-        puts 'Invalid option. Please try again.'
-      end
+      handle_choice(choice)
+      break if choice == 7
     end
+    puts 'Thank you for using the Console Library App. Goodbye!'
   end
 
   private
+
+  def display_options
+    puts "\nPlease choose an option:"
+    puts '1. List all books'
+    puts '2. List all people'
+    puts '3. Create a person'
+    puts '4. Create a book'
+    puts '5. Create a rental'
+    puts '6. List rentals for a person'
+    puts '7. Quit'
+  end
+
+  def handle_choice(choice)
+    case choice
+    when 1 then list_all_books
+    when 2 then list_all_people
+    when 3 then create_person
+    when 4 then create_book
+    when 5 then create_rental
+    when 6 then list_rentals_for_person
+    else
+      puts 'Invalid option. Please try again.'
+    end
+  end
 
   def list_all_books
     if @books.empty?
@@ -76,22 +70,31 @@ class App
     is_teacher = gets.chomp.downcase == 'y'
 
     if is_teacher
-      print 'Specialization: '
-      specialization = gets.chomp
-      person = Teacher.new(age, name, specialization)
+      create_teacher(name, age)
     else
-      print 'Classroom label: '
-      classroom_label = gets.chomp
-      classroom = find_classroom_by_label(classroom_label)
-      if classroom.nil?
-        puts 'Classroom not found. Creating a new one.'
-        classroom = Classroom.new(classroom_label)
-      end
-      person = Student.new(classroom)
+      create_student(name, age)
     end
+  end
 
+  def create_teacher(name, age)
+    print 'Specialization: '
+    specialization = gets.chomp
+    person = Teacher.new(age, name, specialization)
     @people << person
-    puts 'Person created successfully.'
+    puts 'Teacher created successfully.'
+  end
+
+  def create_student(_name, _age)
+    print 'Classroom label: '
+    classroom_label = gets.chomp
+    classroom = find_classroom_by_label(classroom_label)
+    if classroom.nil?
+      puts 'Classroom not found. Creating a new one.'
+      classroom = Classroom.new(classroom_label)
+    end
+    person = Student.new(classroom)
+    @people << person
+    puts 'Student created successfully.'
   end
 
   def create_book
@@ -124,10 +127,10 @@ class App
       return
     end
 
-    print 'Rental date: '
-    rental_date = gets.chomp
+    print 'Rental date (YYYY-MM-DD): '
+    date = gets.chomp
 
-    rental = person.add_rental(book, rental_date)
+    rental = Rental.new(date, book, person)
     @rentals << rental
 
     puts 'Rental created successfully.'
@@ -164,9 +167,7 @@ class App
 
   def find_classroom_by_label(classroom_label)
     @people.each do |person|
-      if person.is_a?(Student) && person.classroom.label == classroom_label
-        return person.classroom
-      end
+      return person.classroom if person.is_a?(Student) && person.classroom.label == classroom_label
     end
     nil
   end
